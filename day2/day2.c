7372bc06694ec507143d1b3d6bc6a34d7f591176
Danny Shoun <github@shoun.dev>
2023-12-01 23:19:37 -0700

    Day 2 Part 2.

--- day2/day2.c
+++ day2/day2.c
@@ -23,7 +23,6 @@ int day2part1() {
 	fopen_s(&file, "./day2/input.txt", "r");
 	if (file) {
 		while ((c = getc(file)) != EOF) {
-			putchar(c);
 			if (c == ':') {
 				past_game_id = 1;
 			} else if (past_game_id == 1) {
@@ -78,5 +77,70 @@ int day2part1() {
 }
 
 int day2part2() {
+	int c;
+	int game_id = 1;
+	int past_game_id = 0;
+	char* curr_num_str = "";
+	char* curr_color = "";
+	int curr_max_red = 0;
+	int curr_max_green = 0;
+	int curr_max_blue = 0;
+	int sum = 0;
+
+	FILE* file;
+	fopen_s(&file, "./day2/input.txt", "r");
+	if (file) {
+		while ((c = getc(file)) != EOF) {
+			if (c == ':') {
+				past_game_id = 1;
+			} else if (past_game_id == 1) {
+				if (c == ',' || c == ';') {
+					curr_num_str[0] = 0;
+					curr_color[0] = 0;
+				} else if (isdigit(c)) {
+					size_t curr_num_str_len = strlen(curr_num_str);
+					char* new_num_str = malloc(curr_num_str_len + 1);
+					strcpy_s(new_num_str, curr_num_str_len + 1, curr_num_str);
+					new_num_str[curr_num_str_len] = c;
+					new_num_str[curr_num_str_len + 1] = '\0';
+					curr_num_str = malloc(strlen(new_num_str) + 1);
+					strcpy_s(curr_num_str, strlen(new_num_str) + 1, new_num_str);
+				} else if (isalpha(c)) {
+					size_t curr_color_len = strlen(curr_color);
+					char* new_color = malloc(curr_color_len + 1);
+					strcpy_s(new_color, curr_color_len + 1, curr_color);
+					new_color[curr_color_len] = c;
+					new_color[curr_color_len + 1] = '\0';
+					curr_color = malloc(strlen(new_color) + 1);
+					strcpy_s(curr_color, strlen(new_color) + 1, new_color);
+
+					if (strcmp(curr_color, "red") == 0) {
+						if (strtol(curr_num_str, NULL, 10) > curr_max_red) {
+							curr_max_red = strtol(curr_num_str, NULL, 10);
+						}
+					} else if (strcmp(curr_color, "green") == 0) {
+						if (strtol(curr_num_str, NULL, 10) > curr_max_green) {
+							curr_max_green = strtol(curr_num_str, NULL, 10);
+						}
+					} else if (strcmp(curr_color, "blue") == 0) {
+						if (strtol(curr_num_str, NULL, 10) > curr_max_blue) {
+							curr_max_blue = strtol(curr_num_str, NULL, 10);
+						}
+					}
+				} else if (c == '\n') {
+					sum += (curr_max_red * curr_max_green * curr_max_blue);
+
+					game_id += 1;
+					past_game_id = 0;
+					curr_num_str[0] = 0;
+					curr_color[0] = 0;
+					curr_max_red = 0;
+					curr_max_green = 0;
+					curr_max_blue = 0;
+				}
+			}
+		}
+	}
+	printf("%i", sum);
 	return 0;
 }
